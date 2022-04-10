fdac9820e8305b02f91592f3d8ebab8ac63fd26e
Seoyeong Park <spark43@ncsu.edu>
2022-04-10 14:50:42 -0400

    add profiles

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AI project
 ## Course scheduler
+
+### Execution
+python3 csp.py <filename without filetype>
+e.g python3 csp.py profile1
---
+++ csp.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+class csp:
+    def __init__(self, degree:str, status:str, min_credit:int, max_credit:int, course_taken_unit:list, course_request_unit:list):
+        self.degree = degree
+        self.status = status
+        self.min_credit = min_credit
+        self.max_credit = max_credit
+        self.course_taken_unit = course_taken_unit
+        self.course_request_unit = course_request_unit
+        print(degree, status, min_credit, max_credit, course_taken_unit, course_request_unit)
+    
+def read_profile(filename):
+    file = "constraints/" + filename + '.txt'
+    with open(file) as file:
+        profile = file.read().replace('\n',' ').split('#')
+    for i in profile[1:]:
+        temp = i.split(' ')
+        if temp[0] == 'degree':
+            degree = temp[1]
+        elif temp[0] == 'degree_status':
+            status = temp[1]
+        elif temp[0] == 'min_credit':
+            min_credit = temp[1]
+        elif temp[0] == 'max_credit':
+            max_credit = temp[1]
+        elif temp[0] == 'course_taken_unit':
+            course_taken = []
+            for t in temp[1:]:
+                if len(t) > 0:
+                    dic = {}
+                    dic['taken'] = t.split('_')[0]
+                    dic['unit'] = t.split('_')[1]
+                    course_taken.append(dic)
+            course_taken_unit = course_taken
+        elif temp[0] == 'course_request_unit':
+            course_request = []
+            for t in temp[1:]:
+                if len(t) > 0:
+                    dic = {}
+                    dic['request'] = t.split('_')[0]
+                    dic['unit'] = t.split('_')[1]
+                    course_request.append(dic)
+            course_request_unit = course_request
+    return degree, status, min_credit, max_credit, course_taken_unit, course_request_unit
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    degree, status, min_credit, max_credit, course_taken_unit, course_request_unit = read_profile(filename)
+    csp = csp(degree, status, min_credit, max_credit, course_taken_unit, course_request_unit)
